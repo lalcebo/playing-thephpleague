35d88777fe50e9915e8688ac5c266244300a56cd
Jorge P. Hernandez Lalcebo <lalcebo2003@gmail.com>
2024-04-28 17:44:17 -0400

    style: pint.

--- app/Events/ExampleEvent.php
+++ app/Events/ExampleEvent.php
@@ -7,7 +7,7 @@ namespace App\Events;
 final class ExampleEvent
 {
     /**
-     * @param array<mixed> $data
+     * @param  array<mixed>  $data
      */
     public function __construct(public array $data = [])
     {
