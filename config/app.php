ee87040d46193a564103c3a677b86468f70bc0fc
Jorge P. Hernandez Lalcebo <lalcebo2003@gmail.com>
2024-04-30 15:09:38 -0400

    feat: add app env setting.

--- config/app.php
+++ config/app.php
@@ -3,7 +3,8 @@
 declare(strict_types=1);
 
 return [
-    'name' => env('APP_NAME', 'The PHP League'),
+    'name' => env('APP_NAME', 'League Framework'),
+    'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
     'timezone' => env('APP_TIMEZONE', 'UTC'),
 ];
